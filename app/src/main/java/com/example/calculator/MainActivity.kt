f49449bac07888fde3936b302186a0233fd9e140
ReginaChege <reginachegewairimu@gmail.com>
2023-07-05 08:27:14 +0300

    changes

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,73 +5,102 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var tiltitle: TextView
-    lateinit var tilfirst: TextView
-    lateinit var tilsecond: TextView
-    lateinit var tiloutput: TextView
-    lateinit var tilbutton1: Button
-    lateinit var tilbtn2: Button
-    lateinit var tilbtn3: Button
-    lateinit var tilbtn4: Button
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tilfirst = findViewById(R.id.tilfirst)
-        tilsecond = findViewById(R.id.tilbtn2)
-        tiloutput = findViewById(R.id.tiloutput)
-        addNumbers()
-        subNumbers()
-        multiplyNumbers()
-        divideNumbers()
-    }
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tilbutton1.setOnClickListener {
 
-    fun addNumbers() {
-        var first = findViewById<TextView>(R.id.tilfirst)
-        var second = findViewById<TextView>(R.id.tilseconnd)
-        var button = findViewById<Button>(R.id.tilbutton1)
-        button.setOnClickListener {
-            var sumfirst = first.text.toString().toInt()
-            var sumsecond = second.text.toString().toInt()
-            var output = sumfirst + sumsecond
-            Toast.makeText(this, "$output", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    override fun onResume(){
+        super.onResume()
+        binding.tilbutton1.setOnClickListener {
+            calculate("+")
+        }
+        binding.tilbutton1.setOnClickListener {
+            calculate("%")
+        }
+        binding.tilbutton1.setOnClickListener {
+            calculate("-")
+        }
+        binding.tilbutton1.setOnClickListener {
+            calculate("button")
         }
     }
 
-    fun subNumbers() {
-        var first = findViewById<TextView>(R.id.tilfirst)
-        var second=findViewById<TextView>(R.id.tilseconnd)
-        var button = findViewById<Button>(R.id.tilbtn4)
-        button.setOnClickListener {
-            var sub = first.text.toString().toInt()
-            var sub2 = second.text.toString().toInt()
-            var output = sub - sub2
-            Toast.makeText(this, "$output", Toast.LENGTH_SHORT).show()
+//    binding.tilbtn2.setOnClickListener {
+//        calculate("m")
+//    }
+//    binding.tilbtn3.setOnClickListener { multiply(validate()) }
+//    tilbtn4.setOnClickListener { modulas() }
+
+
+    }
+    fun calculate(operator:String){
+
+        binding.tilfirst.error=null
+        binding.tilsecond.error=null
+        var second = tilfirst.text.toString()
+        var first = tilsecond.text.toString()
+        var error=false
+        if (first.isBlank()) {
+            binding.tilfirst.error = "first is required"
+//            error = true
+//        }
+//        if (second.isBlank()) {
+//            binding.tilsecond.error = "second is required"
+//            error = true
+//        }
+        if (!error) {
+            var num1=first.toInt()
+            var num2=second.toInt()
+            var result=when(operator){
+                "+"->first.toInt()+second.toInt()
+                "%"->first.toInt()%second.toInt()
+                "buuton"->first.toInt()*second.toInt()
+                "-"->first.toInt()-second.toInt()
+                else->throw Exception("Invalid operation")
+//            return Values(first=first.toInt(),second.toInt())
         }
+        }
+
     }
 
-    fun multiplyNumbers() {
-        var first = findViewById<TextView>(R.id.tilfirst)
-        var second = findViewById<TextView>(R.id.tilseconnd)
-        var button = findViewById<Button>(R.id.tilbtn2)
-        button.setOnClickListener {
-            var multi = first.text.toString().toInt()
-            var multi2 = second.text.toString().toInt()
-            var output = multi * multi2
-            Toast.makeText(this, "$output", Toast.LENGTH_SHORT).show()
-        }
-    }
-        fun divideNumbers() {
-            var first = findViewById<TextView>(R.id.tilfirst)
-            var second = findViewById<TextView>(R.id.tilseconnd)
-            var button = findViewById<Button>(R.id.tilbtn3)
-            button.setOnClickListener {
-                var divide = first.text.toString().toInt()
-                var divide2 = second.text.toString().toInt()
-                var output = divide % divide2
-                Toast.makeText(this, "$output", Toast.LENGTH_SHORT).show()
-            }
-        }
-
+//    fun add(values:Values?){
+//        if(values!=null) {
+//            var sum = values.first + values.second
+//            binding. tilbutton1.text=(sum.toString())
+//
+//        }
+//
+//    }
+//    fun differnce(value:Values?){
+//        if(values!=null) {
+//            var sum = values.first + values.second
+//            binding.tilbutton1.setText(differnce.toString())
+//
+//        }
+//    }
+//    fun multiply(values:values?){
+//        if(values!=null) {
+//            var sum = values.first + values.second
+//            binding.tilbutton1.setText(sum.toString())
+//            binding.tilbutton1.setText(sum.toString())
+//
+//        }
+//    }
+//    fun modulas(Values:values?){
+//        if(values!=null) {
+//            var sum = values.first + values.second
+//            binding.tilbutton1.setText(sum.toString())
+//
+//        }
 }
